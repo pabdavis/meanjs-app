f4256ff3f880e237af935b70b96e78d1acbc133b
Bill Davis <pabdavis@gmail.com>
2014-11-14 16:33:14 -0500

    Change to look for env mongo urls before localhost

--- config/env/secure.js
+++ config/env/secure.js
@@ -2,7 +2,7 @@
 
 module.exports = {
 	port: 443,
-	db: process.env.MONGOHQ_URL || process.env.MONGOLAB_URI || 'mongodb://localhost/mean',
+    db: process.env.COMPOSE_MONGO_URL || process.env.MONGOLAB_URI || 'mongodb://' + (process.env.DB_1_PORT_27017_TCP_ADDR || 'localhost') + '/mean',
 	assets: {
 		lib: {
 			css: [
